eb1823fa854aad9687d1b4116f625675c8134dc6
TzuYung Chen <peter591973@gmail.com>
2023-12-19 04:33:21 +0800

    1. Apply new image_loader to main.py 2. Fix the mistake of giving wrong type ( str -> int) on batch_size and latent_size args

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 # import loader
 import argparse
 import math
-from image_loader import image_process
+from image_loader import *
 from models import *
-import numpy as np
 
 if __name__ == "__main__":
     parser =  argparse.ArgumentParser()
     parser.add_argument("-d", "--dir", required = True, type = str)
     parser.add_argument("-s", "--size", default = 256, type = int)
-    parser.add_argument("-l", "--latent_size", default = 100, type = str)
-    parser.add_argument("-b", "--batch_size", default = 32, type = str)
+    parser.add_argument("-l", "--latent_size", default = 100, type = int)
+    parser.add_argument("-b", "--batch_size", default = 32, type = int)
 
     args = parser.parse_args()
     
-    data = image_process(args.dir, (args.size, args.size))
-    data = np.array(data)
+    data = ImageLoader(args.dir, (args.size, args.size), args.batch_size)
     
     generator = define_generator(args.latent_size, args.size)
     discriminator = define_discriminator(args.size)
@@ -27,6 +25,6 @@ if __name__ == "__main__":
 
     model.compile(tf.keras.optimizers.RMSprop(learning_rate = 1e-4),
                   tf.keras.optimizers.RMSprop(learning_rate = 1e-4))
-    model.fit(data, epochs = 10000, batch_size = int(args.batch_size), callbacks = [Monitor(10, __file__.replace("main.py", ""))], shuffle = False)
+    model.fit(data, epochs = 10000, batch_size = args.batch_size, callbacks = [Monitor(10, __file__.replace("main.py", ""))], shuffle = False)
 
     
